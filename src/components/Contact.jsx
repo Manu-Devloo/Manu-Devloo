d131d8c6d3030ad37d009949e1d8681e9b45f0d8
Manu Devloo <devloo.prive@gmail.com>
2025-09-30 09:25:13 +0200

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -169,7 +169,7 @@ function Contact({ resumeData }) {
             
             <input type="hidden" name="form-name" value="contact" />
             <p className="hidden" style={{ display: 'none' }}>
-              <label>Don't fill this out if you're human: <input name="bot-field" /></label>
+              <label>Don&apos;t fill this out if you&apos;re human: <input name="bot-field" /></label>
             </p>
             
             <Form.Group className="mb-3">
